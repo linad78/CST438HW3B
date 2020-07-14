ad31045ce8ccb3d2c90526743d0dff5fa1c24e65
linadeen <linadeen@localhost>
2020-07-13 22:45:15 -0700

    first commit

---
+++ CST438h-3B/src/main/java/CSt438hw3/Cst438h3ReservationApplication.java
@@ -0,0 +1,13 @@
+package CSt438hw3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cst438h3ReservationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cst438h3ReservationApplication.class, args);
+	}
+
+}
---
+++ CST438h-3B/src/test/java/CSt438hw3/Cst438h3ReservationApplicationTests.java
@@ -0,0 +1,13 @@
+package CSt438hw3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cst438h3ReservationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
